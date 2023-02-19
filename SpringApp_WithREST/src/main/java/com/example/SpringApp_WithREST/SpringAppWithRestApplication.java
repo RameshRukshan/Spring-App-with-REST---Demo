278646b24c4b4631505c98805396048515b0191a
RameshRukshan <ramesharukshan3@gmail.com>
2023-02-19 16:34:57 +0530

    Files ext

---
+++ SpringApp_WithREST/src/main/java/com/example/SpringApp_WithREST/SpringAppWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringApp_WithREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppWithRestApplication.class, args);
+	}
+
+}
---
+++ SpringApp_WithREST/src/test/java/com/example/SpringApp_WithREST/SpringAppWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringApp_WithREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
